100510d19626b7288820f6107bfa8ee82be29c35
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-05 15:03:38 -0300

    Second assignment

--- assignment-02/src/app/app.component.ts
+++ assignment-02/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public username: string;
+
+  /**
+   * isUsernameEmpty
+   *
+   * True if username is an empty string; false otherwise
+   */
+  public isUsernameEmpty(): boolean {
+    return this.username === '';
+  }
+
+  /**
+   * resetUsername
+   */
+  public resetUsername() {
+    this.username = '';
+  }
 }
--- assignment-02/src/app/app.module.ts
+++ assignment-02/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, // Enables ngModel directive
     HttpModule
   ],
   providers: [],
