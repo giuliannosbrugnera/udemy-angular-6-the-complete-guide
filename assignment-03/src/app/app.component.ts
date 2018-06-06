7bc10a82903a116adc019701dfd9ce0e4f21cf95
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-06 13:23:04 -0300

    Files for the third assignment

--- assignment-03/src/app/app.component.ts
+++ assignment-03/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private _showParagraph: boolean;
+  private _buttonClickLogList: Date[] = [];
+
+  public onClickToggleParagraphDisplaying() {
+    this._showParagraph = !this.showParagraph;
+    this._buttonClickLogList.push(new Date());
+  }
+
+  public get showParagraph(): boolean {
+    return this._showParagraph;
+  }
+
+  public get buttonClickLogList(): Date[] {
+    return this._buttonClickLogList;
+  }
+
 }
