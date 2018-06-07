0449330f6c31baca47e5cbfeaf900b193735b63d
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-07 13:42:45 -0300

    Adding a shopping list edit section

--- project-app/src/app/shopping-list/shopping-list.component.ts
+++ project-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+    private _ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatos', 10)
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    public get ingredients(): Ingredient[] {
+        return this._ingredients;
+    }
 }
