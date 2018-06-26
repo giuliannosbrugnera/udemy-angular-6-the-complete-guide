379f9fbe91dfec1c01473cb5c305f4309d616548
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-26 11:23:07 -0300

    Input counter to both Odd and Even components

--- assignment-04/src/app/even/even.component.ts
+++ assignment-04/src/app/even/even.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-even',
   templateUrl: './even.component.html',
   styleUrls: ['./even.component.css']
 })
-export class EvenComponent {}
+export class EvenComponent {
+  @Input() public evenNumber: number;
+}
