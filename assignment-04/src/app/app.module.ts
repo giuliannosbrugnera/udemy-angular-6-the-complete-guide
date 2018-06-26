cf980d304f0cc4e969c6b6ac149bfb0060584233
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-26 08:48:37 -0300

    Create even component

--- assignment-04/src/app/app.module.ts
+++ assignment-04/src/app/app.module.ts
@@ -6,9 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
-  declarations: [AppComponent, GameControlComponent, OddComponent],
+  declarations: [
+    AppComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
+  ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ assignment-04/src/app/even/even.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  templateUrl: './even.component.html',
+  styleUrls: ['./even.component.css']
+})
+export class EvenComponent {}
