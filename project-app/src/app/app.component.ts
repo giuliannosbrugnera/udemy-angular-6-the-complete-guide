a65cfda023b9ca353c7b661670850126e89980ec
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-28 11:53:28 -0300

    Adding navigation with event binding and ngIf

--- project-app/src/app/app.component.ts
+++ project-app/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public selectedFeature: string;
+
+    /**
+     * onFeatureSelected
+     */
+    public onFeatureSelected(feature: string) {
+        this.selectedFeature = feature;
+    }
 }
--- project-app/src/app/header/header.component.ts
+++ project-app/src/app/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: 'header.component.html',
-  styleUrls: ['header.component.css']
+    selector: 'app-header',
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    @Output()
+    private featureSelected: EventEmitter<string> = new EventEmitter<string>();
+
+    public onSelect(feature: string): void {
+        this.featureSelected.emit(feature);
+    }
+}
