818a165179a6fde27dc14ba1abb4455c0d1ce96d
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-28 14:20:48 -0300

    Passing data to Recipe Detail with event and property binding combined

--- project-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,30 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  private _recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://edge.alluremedia.com.au/m/k/2016/08/passintotheiris.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://edge.alluremedia.com.au/m/k/2016/08/passintotheiris.jpg'
-    )
-  ];
+    public recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://edge.alluremedia.com.au/m/k/2016/08/passintotheiris.jpg'
+        ),
+        new Recipe(
+            'Another Test Recipe',
+            'This is simply the second test',
+            'https://gamegator.net/thumbnails/blog-overwatch-brigitte-big.jpg'
+        )
+    ];
 
-  constructor() {}
+    @Output()
+    private recipeSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
-  ngOnInit() {}
+    constructor() {}
 
-  public get recipes(): Recipe[] {
-    return this._recipes;
-  }
+    ngOnInit() {}
+
+    /**
+     * onSelect
+     */
+    public onSelect(recipeElement: Recipe): void {
+        this.recipeSelected.emit(recipeElement);
+    }
 }
--- project-app/src/app/recipes/recipes.component.ts
+++ project-app/src/app/recipes/recipes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+    selector: 'app-recipes',
+    templateUrl: './recipes.component.html',
+    styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+    public recipeSelected: Recipe;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    /**
+     * onRecipeSelected
+     */
+    public onRecipeSelected(recipe: Recipe) {
+        this.recipeSelected = recipe;
+    }
 }
