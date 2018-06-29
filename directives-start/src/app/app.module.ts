3b8726fd05607e106ef3a769681c4acd5b1b9e41
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-29 11:20:08 -0300

    Using the renderer to build a better attribute directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BasicHighlightDirective } from './directives/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ directives-start/src/app/directives/better-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+    selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+    // Arguments will bind to a private property, assigning its value
+    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+    ngOnInit() {
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+    }
+}
