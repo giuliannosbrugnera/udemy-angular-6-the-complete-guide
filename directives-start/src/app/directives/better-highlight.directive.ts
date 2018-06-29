774b5339f6e4aaf38e9233ba084b3d382e2a8a69
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-06-29 14:09:06 -0300

    Using HostBinding to bind to host properties

--- directives-start/src/app/directives/better-highlight.directive.ts
+++ directives-start/src/app/directives/better-highlight.directive.ts
@@ -1,23 +1,34 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import {
+  Directive,
+  Renderer2,
+  OnInit,
+  ElementRef,
+  HostListener,
+  HostBinding
+} from '@angular/core';
 
 @Directive({
-    selector: '[appBetterHighlight]'
+  selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-    // Arguments will bind to a private property, assigning its value
-    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
 
-    ngOnInit() {
-        // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
-    }
+  // Arguments will bind to a private property, assigning its value
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-    @HostListener('mouseenter')
-    mouseOver(eventData: Event) {
-        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
-    }
+  ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+  }
 
-    @HostListener('mouseleave')
-    mouseLeave(eventData: Event) {
-        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    }
+  @HostListener('mouseenter')
+  mouseOver(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+    this.backgroundColor = 'orange';
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
+  }
 }
