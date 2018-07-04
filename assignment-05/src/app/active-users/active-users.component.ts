cc1caec2628e0819447d0c8af45e7febdb567fc6
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-07-04 15:55:51 -0300

    Refactoring

--- assignment-05/src/app/active-users/active-users.component.ts
+++ assignment-05/src/app/active-users/active-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
 
 @Component({
@@ -6,12 +6,15 @@ import { UsersService } from '../services/users.service';
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
+export class ActiveUsersComponent implements OnInit {
   public users: string[];
 
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService) {}
+
+  // Good practice to initialize OnInit
+  ngOnInit() {
+    // Assignment by reference. Changes on UsersService will reflect to this.users
     this.users = this.usersService.activeUsers;
-    this.usersService.activeUsersUpdated.subscribe((activeUsers: string[]) => this.users = activeUsers);
   }
 
   onSetToInactive(id: number) {
--- assignment-05/src/app/inactive-users/inactive-users.component.ts
+++ assignment-05/src/app/inactive-users/inactive-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
 
 @Component({
@@ -6,12 +6,15 @@ import { UsersService } from '../services/users.service';
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
+export class InactiveUsersComponent implements OnInit {
   public users: string[];
 
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService) {}
+
+  // Good practice to initialize OnInit
+  ngOnInit() {
+    // Assignment by reference. Changes on UsersService will reflect to this.users
     this.users = this.usersService.inactiveUsers;
-    this.usersService.inactiveUsersUpdated.subscribe((inactiveUsers: string[]) => this.users = inactiveUsers);
   }
 
   onSetToActive(id: number) {
