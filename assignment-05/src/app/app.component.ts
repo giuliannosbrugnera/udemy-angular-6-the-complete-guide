35fd074b2ce8b45e3e0283ca488894eba1446de2
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-07-04 15:37:57 -0300

    Outputting operations counter

--- assignment-05/src/app/app.component.ts
+++ assignment-05/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { UsersService } from './services/users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private usersService: UsersService) {}
+
+  get operations(): number {
+    return this.usersService.toggleStatusCounter || 0;
+  }
 }
