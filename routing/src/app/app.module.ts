507c549eb3b3cca61477e84ccc912d8fd6e3a73d
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-07-11 15:34:24 -0300

    Fetching route parameters

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:id', component: UserComponent }, // parameters: dynamic segments in the path
+  { path: 'users/:id/:name', component: UserComponent }, // parameters: dynamic segments in the path
   { path: 'servers', component: ServersComponent }
 ];
 
