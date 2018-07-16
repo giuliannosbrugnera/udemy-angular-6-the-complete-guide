18c640b876b7d5e4aeb5672f980f7c324014e6bf
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-07-16 13:30:04 -0300

    Adding navigation to the app

--- project-app/src/app/header/header.component.ts
+++ project-app/src/app/header/header.component.ts
@@ -1,15 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html',
     styleUrls: ['header.component.css']
 })
-export class HeaderComponent {
-    @Output()
-    private featureSelected: EventEmitter<string> = new EventEmitter<string>();
-
-    public onSelect(feature: string): void {
-        this.featureSelected.emit(feature);
-    }
-}
+export class HeaderComponent {}
