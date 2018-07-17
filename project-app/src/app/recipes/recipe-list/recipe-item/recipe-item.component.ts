33942675c86b3fd7b834c2549964383c23af7414
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-07-17 08:30:35 -0300

    Passing dynamic parameters to links

--- project-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ project-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
     selector: 'app-recipe-item',
@@ -11,11 +10,7 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
     @Input() public recipe: Recipe;
 
-    constructor(private recipeService: RecipeService) {}
+    constructor() {}
 
     ngOnInit() {}
-
-    onSelected() {
-        this.recipeService.recipeSelected.emit(this.recipe);
-    }
 }
