32488732d7b3433e1ec81c6f69f4e2315d218062
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-07-18 12:56:21 -0300

    Resetting forms

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -54,5 +54,6 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
     this.submitted = true;
+    this.signupForm.reset();
   }
 }
